cb8d7dcc104f9cc77d523d0f3f52d56b6542df8e
K7 <k7@k7cl.com>
2020-10-20 15:43:45 +0800

    Create rec.java

---
+++ rec.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class rec {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Scanner sc = new Scanner(System.in);
+		int no1 = sc.nextInt();
+		int no2 = -1;
+		float reslut = 1/(float)no1;
+		System.out.print(String.format("%.2f",reslut));
+	}
+}
