d91133d473bda7970140910f7a2d3269f2221c56
K7 <k7@k7cl.com>
2020-10-20 15:46:55 +0800

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sub{
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Scanner sc = new Scanner(System.in);
+		int no1 = sc.nextInt();
+		int no2 = sc.nextInt();
+		int reslut = no1-no2;
+		System.out.println(reslut);
+	}
+}
